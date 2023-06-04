685d0bd32f986e27e2a3bec454d9705b80a46ede
Jesulayomy <jesulayomi081@gmail.com>
2023-06-04 22:55:08 +0000

    Header icon fix

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   const amDict = {};
 
-  $.getJSON('http://0.0.0.0:5000/api/v1/status/', function (data) {
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data.status == 'OK') {
       $('DIV#api_status').addClass('available');
     }
